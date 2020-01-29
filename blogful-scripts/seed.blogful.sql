116b18f5989dacb1d406d74f9abb068790738cea
Jordan Castillo <jordanxcallaway@gmail.com>
2020-01-29 10:59:51 -0800

    blogful_articles TABLE created and seeded with test data

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, content, date_published)
+VALUES 
+  ('Article 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() - '21 days'::INTERVAL),
+  ('Article 2', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ', now() - '21 days'::INTERVAL),
+  ('Article 3', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ', now() - '21 days'::INTERVAL),
+  ('Article 4', 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', now() - '21 days'::INTERVAL),
+  ('Article 5', 'Amet consectetur adipiscing elit pellentesque. Vestibulum morbi blandit cursus risus at ultrices mi. Gravida cum sociis natoque penatibus et magnis dis parturient.', now() - '21 days'::INTERVAL),
+  ('Article 6', 'Gravida arcu ac tortor dignissim convallis aenean et tortor at.Vestibulum morbi blandit cursus risus at ultrices mi. Gravida cum sociis natoque penatibus et magnis dis parturient.', now() - '21 days'::INTERVAL),
+  ('Article 7', 'Morbi blandit cursus risus at ultrices mi tempus.', now() - '21 days'::INTERVAL),
+  ('Article 8', 'Vestibulum morbi blandit cursus risus at ultrices mi. Gravida cum sociis natoque penatibus et magnis dis parturient.', now() - '21 days'::INTERVAL),
+  ('Article 9', 'Duis at tellus at urna. Enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra.', now() - '21 days'::INTERVAL),
+  ('Article 10', 'Duis at tellus at urna. Enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() - '21 days'::INTERVAL),
+  ('Article 11', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() - '21 days'::INTERVAL),
+  ('Article 12', 'Consectetur purus ut faucibus pulvinar. Tristique et egestas quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() - '21 days'::INTERVAL),
+  ('Article 13', 'Dignissim convallis aenean et tortor at risus viverra adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() - '21 days'::INTERVAL),
+  ('Article 14', 'Dui ut ornare lectus sit amet est. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque.', now() - '21 days'::INTERVAL),
+  ('Article 15', 'Dui ut ornare lectus sit amet est. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() - '21 days'::INTERVAL),
+  ('Article 16', 'Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Enim sed faucibus turpis in. Eu mi bibendum neque egestas congue quisque.', now() - '21 days'::INTERVAL),
+  ('Article 17', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() - '21 days'::INTERVAL),
+  ('Article 18', 'Ultricies integer quis auctor elit sed. Blandit libero volutpat sed cras ornare arcu. Tortor id aliquet lectus proin. Cursus eget nunc scelerisque viverra mauris.', now() - '21 days'::INTERVAL),
+  ('Article 19', 'Faucibus ornare suspendisse sed nisi lacus sed viverra tellus in. Donec ac odio tempor orci dapibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', now() - '21 days'::INTERVAL),
+  ('Article 20', 'Vulputate dignissim suspendisse in est. Egestas diam in arcu cursus euismod quis', now() - '21 days'::INTERVAL)
+;
---
+++ src/drills.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+function searchItem(searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .first()
+    .then(res => {
+      console.log(res);
+      console.log('SEARCH TERM:', {searchTerm});
+    });
+}
+
+//searchItem('kale');
+
+function paginateShoppingItems(page) {
+  const itemsPerPage = 6;
+  const offset = itemsPerPage * (page -1);
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .limit(itemsPerPage)
+    .offset(offset)
+    .then(res => {
+      console.log(res);
+      console.log('Page number:', {page});
+    });
+}
+
+//paginateShoppingItems(2);
+
+function itemsAddedDaysAgo(daysAgo){
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('date_added', '>', knexInstance.raw(`now() - '?? days':: INTERVAL`, daysAgo))
+    .then(res => {
+      console.log(res);
+      console.log('Days Ago:', {daysAgo});
+    });
+}
+
+//itemsAddedDaysAgo(3);
+
+function totalCostofCategories () {
+  knexInstance
+    .select('category')
+    .from('shopping_list')
+    .sum('price AS sum')
+    .groupBy('category')
+    .then(res => {
+      console.log(res);
+    });
+}
+
+totalCostofCategories();
